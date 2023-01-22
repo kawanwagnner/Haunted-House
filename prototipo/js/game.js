17e7cbd3a85dc59f6c1d09ccf5d9b7c1e6e41a7b
Kawan Wagnner <kawanwagnner7@gmail.com>
2023-01-22 17:37:04 -0300

    Fisrt commit of game  Haunted House(protótipo)

---
+++ prototipo/js/game.js
@@ -0,0 +1,29 @@
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+const canvasWidth = 1024;
+const canvasHeight = 576;
+
+canvas.width = canvasWidth;
+canvas.height = canvasHeight;
+
+let prevTime = 0
+
+animate();
+
+function animate() {
+    window.requestAnimationFrame(animate);
+
+    handleControls()
+
+    ctx.fillStyle = "#000";
+    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+
+    background.update()
+    player.update()
+
+    let delta = (performance.now() - prevTime) / 1000;
+    let fps = 1 / delta;
+
+    prevTime = performance.now();
+};
